56c4957bf0e2156d76b96f62a7777da1379445c0
Martin Boekhoff <martinb@ptscientists.com>
2018-08-07 21:39:15 +0200

    clean up and comit missing files

--- borgware-arduino/animations/blackhole.h
+++
@@ -1,6 +0,0 @@
-#ifndef BLACKHOLE_H_
-#define BLACKHOLE_H_
-
-void blackhole(void);
-
-#endif /* BLACKHOLE_H_ */
--- borgware-arduino/util.h
+++
@@ -1,7 +0,0 @@
-#ifndef UTIL_H
-#define UTIL_H
-
-
-void b2d_wait(int ms);
-
-#endif
